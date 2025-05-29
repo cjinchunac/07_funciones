f896c6a8bf89fd3f3110bfe2af8af886ae7cdfa1
cjinchunac <cjinchunac@unjbg.edu.pe>
2025-05-28 23:24:11 -0500

    Actualiza ejercicio1: Agrega mensajes y abrevia variables

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -3,33 +3,38 @@ using namespace std;
 void dividir(int val1, int val2);
 
 int main () {
-	int a, b, mayor, menor;
+	int a, b, ma, me;
+	cout<<"=== DIVISION ENTERA CON RESTO ==="<<endl;
+	cout<<"Este programa calcula el cociente y el resto de una division entre dos numeros"<<endl<<endl;
 	cout<<"Ingrese el primer numero: ";
 	cin>>a;
 	cout<<"Ingrese el segundo numero: ";
 	cin>>b;
 	if (a>=b) {
-		mayor=a;
-		menor=b;
+		ma=a;
+		me=b;
 	}	
 	else {
-		mayor=b;
-		menor=a;
+		ma=b;
+		me=a;
 	}
-	dividir(mayor, menor);
+	dividir(ma, me);
+	cout<<endl<<endl<<"Gracias por usar este programa";
 	return 0;
 }
 
 void dividir (int val1, int val2) {
-	int cociente, resto, numerador, denominador;
-	numerador=val1;
-	denominador=val2;
-	cociente=0;
+	int c, r, nu, de;
+	nu=val1;
+	de=val2;
+	c=0;
+	if (val2==0)
+		cout<<"NO SE PUEDE DIVIDIR ENTRE 0";
 	while (val1>=val2) {
 		val1=val1-val2;
-		cociente=cociente+1;
-		resto=val1;
+		c=c+1;
+		r=val1;
 	}
-	cout<<"\nEl cociente de la division de "<<numerador<<" / "<<denominador<<" es: "<<cociente;
-	cout<<"\nEl resto de la division de "<<numerador<<" / "<<denominador<<" es: "<<resto;
+	cout<<"\nEl cociente de la division de "<<nu<<" / "<<de<<" es: "<<c;
+	cout<<"\nEl resto de la division de "<<nu<<" / "<<de<<" es: "<<r;
 }
