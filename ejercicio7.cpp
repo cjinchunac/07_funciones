99a55d8c2bf3481c43faca7bc23b11ee0e145d49
cjinchunac <cjinchunac@unjbg.edu.pe>
2025-06-03 16:32:36 -0500

    Actuliza ejercicio7: Mensajes al usuario y solo permite numeros positivos

--- ejercicio7.cpp
+++ ejercicio7.cpp
@@ -4,11 +4,20 @@ int MCD (int, int);
 int MCM (int, int);
 int main () {
 	int num1, num2, mcd, mcm;
-	cin>>num1>>num2;
-	mcd=MCD(num1,num2);
-	mcm=MCM(num1,num2);
-	cout<<"El MCD de estos numeros es: "<<mcd<<endl;
-	cout<<"EL MCM de estos numeros es: "<<mcm<<endl;
+	cout<<"¡¡A CONTINUACION CALCULAREMOS EL MCD Y MCM DE DOS NUMEROS!!"<<endl;
+	cout<<"Usted ingresara dos numeros"<<endl;
+	cout<<"Ingrese el primer numero: ";
+	cin>>num1;
+	cout<<"Ingrese el segundo numero: ";
+	cin>>num2;
+	if (num1>=0&&num2>=0) {
+		mcd=MCD(num1,num2);
+		mcm=MCM(num1,num2);
+		cout<<endl<<"El MCD de estos numeros es: "<<mcd<<endl;
+		cout<<"EL MCM de estos numeros es: "<<mcm<<endl;
+	}
+	else
+		cout<<endl<<"NO HA INGRESADO VALORES VALIDOS";
 	return 0;
 }
 
